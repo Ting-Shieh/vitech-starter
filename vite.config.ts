1bae7e048a3a7536f2c1c17a7fe07ec1d93b7e67
Ting-Shieh <zsting29@gmail.com>
2024-06-10 14:21:05 +0800

    feat: add unplgin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueRouter from 'unplugin-vue-router/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
+import Components from 'unplugin-vue-components/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -35,7 +36,8 @@ export default defineConfig({
         // // custom
         // { }
       ]
-    })
+    }),
+    Components({})
   ],
   resolve: {
     alias: {
